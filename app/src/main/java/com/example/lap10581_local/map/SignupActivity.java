4cd3b75f785f835cd460c7c14956a99606847b3b
kevin nguyen <nkevin1088@gmail.com>
2018-06-05 17:24:47 +0700

    pre final

---
+++ app/src/main/java/com/example/lap10581_local/map/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.lap10581_local.map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
